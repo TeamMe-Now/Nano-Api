c9c5a43499f9978ed0b0efee412459c72aecc953
mael <maelgarnier5@gmail.com>
2024-09-09 21:54:58 -0400

    ajout de commentaires

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+/*
+API gateway faite a partir de ce tuto sur medium : https://medium.com/geekculture/create-an-api-gateway-using-nodejs-and-express-933d1ca23322
+Prochain ajouts :
+- Authentication
+- Rate limiting
+- Real routes to real microservices
+ */
+
 const express = require('express')
 
-const {ROUTES} = require("./routes");
+const {ROUTES} = require("./route");
 
 const {setupLogging} = require("./logging");
 const {setupProxies} = require("./proxy");
@@ -13,5 +21,5 @@ setupLogging(app);
 setupProxies(app, ROUTES);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`The app is now listening at http://localhost:${port}`)
 })
